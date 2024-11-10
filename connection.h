73b203b408ad63b2330904eb04c29dcca990f21a
MayssemChouria27 <mayssemchouria5@gmail.com>
2024-11-10 22:54:47 +0100

    First commit connection.h

--- connection.h
+++ connection.h
@@ -1,14 +1,15 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 #include <QtSql/QSqlDatabase>
-
+#include <QtSql/QSqlError>
+#include <QtSql/QSqlQuery>
 
 class connection
 {
     QSqlDatabase db;
 public:
     connection();
-    bool createconnection();
+    bool createconnect();
     void closeConnection();
 };
 
